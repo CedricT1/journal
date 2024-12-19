20ff9502ec51d8bfeb25c494349f993d5d8a6b06
Cédric Trachsel <cedric@trachsel.org>
2024-12-19 15:09:00 +0100

    correction bug

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,14 @@ def create_app():
 
     db.init_app(app)
 
-    from app import routes, models
+    # Importer les routes ici pour éviter les imports circulaires
+    from app.routes import index, add_rss, get_rss_feeds, generate_bulletin
+
+    # Enregistrer les routes
+    app.add_url_rule('/', 'index', index)
+    app.add_url_rule('/add_rss', 'add_rss', add_rss, methods=['POST'])
+    app.add_url_rule('/get_rss_feeds', 'get_rss_feeds', get_rss_feeds, methods=['GET'])
+    app.add_url_rule('/generate_bulletin', 'generate_bulletin', generate_bulletin, methods=['POST'])
 
     with app.app_context():
         db.create_all()
--- routes.py
+++ routes.py
@@ -1,17 +1,15 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import render_template, request, jsonify
 from app import db
 from app.models import RSSFeed
 import feedparser
 
-# Créer un Blueprint
-bp = Blueprint('main', __name__)
+# Supprimez la création d'une nouvelle instance
+# Les routes seront attachées à l'application via Blueprint ou directement dans create_app
 
-@bp.route('/')
 def index():
     feeds = RSSFeed.query.all()
     return render_template('index.html', feeds=feeds)
 
-@bp.route('/add_rss', methods=['POST'])
 def add_rss():
     data = request.json
     url = data.get('rssUrl')
@@ -36,12 +34,10 @@ def add_rss():
 
     return jsonify({"message": "Flux RSS ajouté avec succès"}), 200
 
-@bp.route('/get_rss_feeds', methods=['GET'])
 def get_rss_feeds():
     feeds = RSSFeed.query.all()
     return jsonify([{"id": feed.id, "url": feed.url, "category": feed.category} for feed in feeds])
 
-@bp.route('/generate_bulletin', methods=['POST'])
 def generate_bulletin():
     # Cette fonction sera implémentée plus tard
     return jsonify({"message": "Génération du bulletin en cours"}), 200
